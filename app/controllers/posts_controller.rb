26ef77f08b4a847bfb9fa1ee12f27afa775edfbb
andrew gibbs <andrewgibbs@outlook.com>
2016-04-14 11:28:40 -0700

    proper showing of post in user click

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
 
   def index
     user_ids = current_user.timeline_user_ids
-    @posts=Post.where(user_id: user_ids).order("created_at DESC")
+    @posts= Post.all.order("created_at DESC")
+    # @posts=Post.where(user_id: user_ids).order("created_at DESC")
 
     # @comments = Comment.where(post_id: post_ids)("created_at DESC")
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order("created_at DESC")
-
+     @posts = @user.posts.order("created_at DESC")
+    # @posts = Post.all.order("created_at DESC")
   end
   def new
     @user = User.new
